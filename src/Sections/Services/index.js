e7a5cc05f1a3d5891e43ffdc12992164e781cebe
heyputnam <heyputnam@gmail.com>
2021-06-01 15:28:31 -0400

    added basic sevice section and text component

--- src/Sections/Services/index.js
+++ src/Sections/Services/index.js
@@ -1,9 +1,105 @@
+import styled from 'styled-components'
+
+const ServiceSection = styled.div`
+
+width: 100vw;
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+padding-top: 20rem;
+position: relative;
+color: var(--white);
+
+
+`
+
+
+const Background = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+position: absolute;
+top: 0;
+left: 0;
+bottom: 0;
+width: 100vw;
+height: 85vh;
+background: var(--black);
+background-size: auto 100vh;
+z-index: -1;
+
+
+
+`
+
+
+const Title = styled.h1`
+display: inline-block;
+font-size: 2rem;
+margin-top: 1rem;
+position:relative;
+
+&::before{
+    content: "";
+    height: 1px;
+    width: 50%;
+    position: absolute;
+    left: 50%;
+    bottom: 0;
+    border-bottom: 2px solid var(--bmwblue);
+    transform: translate(-50%);
+}
+
+
+`
+
+
+const Line = styled.div`
+border-left: 4px solid var(--white);
+height: 15rem;
+margin-top: 2rem;
+border-radius: 20px 20px 0 0;
+
+
+
+`
+
+
+const Triangle = styled.div`
+width: 0;
+height: 0;
+border-left: 1.2rem solid transparent;
+border-right: 1.2rem solid transparent;
+border-top: 2rem solid var(--white);
+
+`
+
+const Content = styled.div`
+
+
+`
+
+
+const TextBlock = styled.div`
+
+
+`
 
 const Services = () => {
     return(
-        <section>
-         Services Section 
-        </section>
+       <ServiceSection>
+           <Background>
+               <Title>What We Do</Title>
+               <Line/>
+               <Triangle/>
+           </Background>
+           <Content>
+               <TextBlock>
+
+               </TextBlock>
+           </Content>
+       </ServiceSection>
     )
 }
 
--- src/components/TextBlock/TextBlock.js
+++ src/components/TextBlock/TextBlock.js
@@ -1,8 +1,112 @@
 
-const TextBlock = () => {
+import styled from 'styled-components'
+import circle from '../../assets/circle.png'
+
+const MainContent = styled.div`
+display: flex;
+ justify-content: center;
+ align-items: center;
+ width: 70vw;
+
+ @media only Screen and (max-width: 48em){
+     flex-direction: column;
+     width: 100vw;
+     justify-content: center;
+     align-items: center;
+ 
+
+
+ }
+ 
+`
+const LeftBlock = styled.div`
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+width: 50%;
+line-height: 1.5;
+color: white;
+@media only Screen and (max-width: 48em){
+   width: 100%;
+   text-align: center;
+   align-items: center;
+}
+
+
+
+
+`
+const Topic = styled.div`
+
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+background-color: var(--bmwblue);
+font-weight: 700;
+font-size: calc(0.3rem + 0.3vw);
+padding: 0.5rem 1rem;
+border-radius: 20px;
+width: calc(4rem + 4vw);
+
+
+
+`
+
+const Circle = styled.img`
+height: 1rem;
+width: 1rem;
+margin-right: 0.3rem;
+
+
+
+
+
+`
+
+const Title = styled.h1`
+font-size: calc(2rem + 1vw);
+line-height: 1.2;
+padding: 0.5rem 0;
+
+
+
+
+`
+
+const SubText = styled.h5`
+
+font-size: calc(0.5rem + 0.5vw);
+ color: var(--bmwblue);
+
+
+
+ 
+`
+
+
+
+const TextBlock = ({topic, title, subText, children}) => {
     return(
         <section>
-            textblock component
+              <MainContent>
+            <LeftBlock>
+                <Topic>
+                    <Circle src={circle}/>
+           
+                    <span>{topic}</span>
+                </Topic>
+                <Title>{title}</Title>
+                <SubText>
+                    {subText}
+                </SubText>
+
+        
+            </LeftBlock>
+           
+            {children}
+        
+            </MainContent>
         </section>
     )
 }
