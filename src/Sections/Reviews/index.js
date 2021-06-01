50ee69583a7acdc579a867ce4c416bfbebff4c68
heyputnam <heyputnam@gmail.com>
2021-06-01 17:02:25 -0400

    styled reviews

--- src/Sections/Reviews/index.js
+++ src/Sections/Reviews/index.js
@@ -10,7 +10,7 @@ align-items: center;
 justify-content: center;
 padding-top: 20rem;
 position: relative;
-color: var(--white);
+color: var(--black);
 
 
 `
@@ -25,7 +25,7 @@ left: 0;
 bottom: 0;
 width: 100vw;
 height: 100%;
-background: var(--black);
+background: var(--white);
 background-size: auto 100vh;
 z-index: -1;
 
@@ -54,6 +54,16 @@ position:relative;
 
 `
 
+const Review = styled.div`
+width: 100%;
+position: relative;
+top: -9rem;
+left: 0;
+
+
+
+`
+
 const Reviews = () => {
     return(
         <ReviewsSection>
@@ -61,9 +71,9 @@ const Reviews = () => {
                <Title>What Others are Saying</Title>
                
            </Background>
-
+            <Review>
              <ElfsightWidget widgetID="0a54b799-c158-4be9-bdd5-a25f73332f30" />
-      
+             </Review>
         </ReviewsSection>
     )
 }
